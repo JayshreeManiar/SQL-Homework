35f1e17df4c3f15c6534027d1f1921e8820e87bb
Jayshree <jayshri123@yahoo.com>
2019-07-17 16:18:38 -0500

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -8,13 +8,13 @@ DROP TABLE IF EXISTS customer_list;
 
 
 CREATE TABLE departments (
-  dept_no VARCHAR NOT NULL,
-  dept_name character varying(40) NOT NULL
+  dept_no VARCHAR NOT NULL AS PRIMARY KEY,
+  dept_name VARCHAR NOT NULL
 );
 
 CREATE TABLE dept_emp (
   emp_no integer NOT NULL,
-  dept_no character varying(20) NOT NULL,
+  dept_no  VARCHAR NOT NULL,
   from_date DATE  NOT NULL,
   to_date DATE NOT NULL
 );
@@ -27,7 +27,7 @@ CREATE TABLE dept_manager (
 );
 
 CREATE TABLE employees (
-    emp_no integer NOT NULL,
+    emp_no integer NOT NULL AS PRIMARY KEY,
     birth_date DATE NOT NULL,
     first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
@@ -38,15 +38,15 @@ CREATE TABLE employees (
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
-  from_date timestamp without time zone DEFAULT now() NOT NULL,
-  to_date timestamp without time zone DEFAULT now() NOT NULL
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL
 );
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  title VARCHAR(50) NOT NULL,
-  from_date timestamp without time zone DEFAULT now() NOT NULL,
-  to_date timestamp without time zone DEFAULT now() NOT NULL
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL
 );
 
 --List the following details of each employee: employee number, last name, first name, gender, and salary.
@@ -54,6 +54,7 @@ CREATE TABLE titles (
 SELECT * FROM salaries;
 SELECT * FROM employees;
 select * FROM dept_emp;
+SELECT * FROM departments;
 
 SELECT E.emp_no, E.last_name,E.first_name,E.gender,S.salary 
 FROM employees AS E
@@ -63,30 +64,19 @@ E.emp_no = S.emp_no;
 --List employees who were hired in 1986.
 
 SELECT * FROM employees
-WHERE hire_date > '1986-01-01' AND hire_date < '1986-12-31' ;
+WHERE hire_date >='1986-01-01' AND hire_date <= '1986-12-31' ;
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 SELECT * FROM dept_manager;
-
-SELECT M.dept_no,M.emp_no,M.from_date,M.to_date, E.first_name,E.last_name
+SELECT * FROM departments;
+--(1)
+SELECT M.dept_no,M.emp_no,M.from_date,M.to_date, Emp.first_name,Emp.last_name, Dep.dept_name
 FROM dept_manager AS M
-INNER JOIN employees AS E ON
-M.emp_no = E.emp_no;
-
-
-
-SELECT emp_no, first_name,last_name FROM employees
-WHERE emp_no IN
-(SELECT dept_no FROM dept_manager);
-
-
-SELECT emp_no,first_name,last_name FROM employees
-WHERE emp_no IN
-(
-SELECT dept_no,from_date,to_date FROM dept_manager
-WHERE dept_no IN
-(SELECT dept_name FROM departments));
-
+JOIN employees AS Emp 
+	ON M.emp_no = Emp.emp_no
+JOIN departments AS Dep
+	ON M.dept_no= Dep.dept_no;
+--(2)
 SELECT * FROM employees
 WHERE emp_no IN
 (SELECT emp_no 
@@ -95,3 +85,57 @@ WHERE dept_no IN
 (SELECT dept_no
 FROM departments));
 
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT * FROM dept_emp;
+
+SELECT M.emp_no, Emp.first_name,Emp.last_name, Dep.dept_name
+FROM dept_emp AS M
+JOIN employees AS Emp 
+	ON M.emp_no = Emp.emp_no
+JOIN departments AS Dep
+	ON M.dept_no= Dep.dept_no;
+	
+--List all employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT * FROM employees
+WHERE first_name  = 'Hercules'
+AND last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT * FROM departments;
+
+SELECT M.emp_no,M.last_name,M.first_name,D.dept_name
+FROM dept_emp AS S
+JOIN employees AS M
+ON M.emp_no = S.emp_no
+JOIN departments as D
+ON S.dept_no = D.dept_no
+WHERE dept_name = 'Sales';
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT M.emp_no,M.last_name,M.first_name,D.dept_name
+FROM dept_emp AS S
+JOIN employees AS M
+ON M.emp_no = S.emp_no
+JOIN departments as D
+ON S.dept_no = D.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT COUNT( DISTINCT last_name) AS "FREQUENCY COUNT OF EMPLOYEE LAST NAME " FROM employees;
+
+SELECT DISTINCT COUNT(last_name) AS "Frequency" , last_name FROM employees
+GROUP BY last_name
+ORDER BY "Frequency" DESC;
+
+---------------------------------------------------------------------------------------------------------
+--EPILOGUE
+
+SELECT * FROM employees
+WHERE emp_no = 499942;
+---------------------------------------------------------------------------------------------------------
